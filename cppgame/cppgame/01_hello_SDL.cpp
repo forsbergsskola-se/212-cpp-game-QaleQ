798caf0dbd4b94fb3546f841e9e2b9a30287260a
QaleQ <aleflard@gmail.com>
2023-04-13 20:43:34 +0200

    clean up bad code from tutorial

--- cppgame/cppgame/01_hello_SDL.cpp
+++ cppgame/cppgame/01_hello_SDL.cpp
@@ -7,10 +7,10 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 //The window we'll be rendering to
-SDL_Window* gWindow = NULL;
+SDL_Window* gWindow = nullptr;
 
 //The window renderer
-SDL_Renderer* gRenderer = NULL;
+SDL_Renderer* gRenderer = nullptr;
 
 int main(int argc, char* args[]) {
     //Initialize SDL
@@ -20,55 +20,51 @@ int main(int argc, char* args[]) {
     else {
         //Create window
         gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (gWindow == NULL) {
+        if (!gWindow) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            return -1;
         }
-        else {
-            //Create renderer for window
-            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-            if (gRenderer == NULL) {
-                printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+
+        //Create renderer for window
+        gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+        if (!gRenderer) {
+            printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+            return -1;
+        }
+        
+        //Initialize renderer color
+        SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+
+        //Initialize the player
+        Dot player{ 0, 0 };
+
+
+        //Event handler
+        SDL_Event e;
+
+        //While application is running
+        while (true) {
+            //Handle events on queue
+            if (SDL_PollEvent(&e)) {
+                //User requests quit
+
+                //Handle input for the dot
+                player.handleEvent(e);
             }
-            else {
-                //Initialize renderer color
-                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-                //Initialize the dot
-                Dot dot;
+            //Move the dot
+            player.move(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-                //Main loop flag
-                bool quit = false;
+            //Clear screen
+            SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+            SDL_RenderClear(gRenderer);
 
-                //Event handler
-                SDL_Event e;
+            //Render objects
+            player.render(gRenderer);
+            target.render(gRenderer);
 
-                //While application is running
-                while (!quit) {
-                    //Handle events on queue
-                    while (SDL_PollEvent(&e) != 0) {
-                        //User requests quit
-                        if (e.type == SDL_QUIT) {
-                            quit = true;
-                        }
-
-                        //Handle input for the dot
-                        dot.handleEvent(e);
-                    }
-
-                    //Move the dot
-                    dot.move(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-                    //Clear screen
-                    SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
-                    SDL_RenderClear(gRenderer);
-
-                    //Render objects
-                    dot.render(gRenderer);
-
-                    //Update screen
-                    SDL_RenderPresent(gRenderer);
-                }
-            }
+            //Update screen
+            SDL_RenderPresent(gRenderer);
         }
     }
 
