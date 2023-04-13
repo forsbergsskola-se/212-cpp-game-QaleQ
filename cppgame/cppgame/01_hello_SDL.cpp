be818a22614e2dd9033739135f2673e470f7ea36
QaleQ <aleflard@gmail.com>
2023-04-13 21:44:25 +0200

    code cleanup

--- cppgame/cppgame/01_hello_SDL.cpp
+++ cppgame/cppgame/01_hello_SDL.cpp
@@ -21,68 +21,67 @@ int main(int argc, char* args[]) {
     //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
     }
-    else {
-        //Create window
-        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (!gWindow) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-            return -1;
-        }
+    //Create window
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!gWindow) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
 
-        //Create renderer for window
-        gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-        if (!gRenderer) {
-            printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-            return -1;
-        }
+    //Create renderer for window
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (!gRenderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
         
-        //Initialize renderer color
-        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
+    //Initialize renderer color
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
 
-        //Initialize the player
-        Dot player{ 0, 0 };
+    //Initialize the player
+    Dot player{ 0, 0 };
 
-        Dot target{ 300, 300 };
+    Dot target{ 300, 300 };
 
-        //Event handler
-        SDL_Event e;
+    //Event handler
+    SDL_Event e;
 
-        //While application is running
-        while (true) {
-            //Handle events on queue
-            if (SDL_PollEvent(&e)) {
-                //User requests quit
-                if (e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE) { break; }
+    //While application is running
+    while (true) {
+        //Handle events on queue
+        if (SDL_PollEvent(&e)) {
+            //User requests quit
+            if (e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE) { break; }
 
-                //Handle input for the dot
-                player.handleEvent(e);
-            }
-
-            //Check if target is reached
-            if (Observer::checkCollision(player, target)) {
-                //Increment score and terminate game loop if MAX_SCORE is reached
-                if (++SCORE == MAX_SCORE) break;
-
-                //Move target 
-                target.mPosX = rand() % (SCREEN_WIDTH - target.DOT_WIDTH);
-                target.mPosY = rand() % (SCREEN_HEIGHT - target.DOT_HEIGHT);
-            }
-
-            //Move the dot
-            player.move(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-            //Clear screen
-            SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
-            SDL_RenderClear(gRenderer);
-
-            //Render objects
-            player.render(gRenderer);
-            target.render(gRenderer);
-
-            //Update screen
-            SDL_RenderPresent(gRenderer);
+            //Handle input for the dot
+            player.handleEvent(e);
         }
+
+        //Check if target is reached
+        if (Observer::checkCollision(player, target)) {
+            //Increment score and terminate game loop if MAX_SCORE is reached
+            if (++SCORE == MAX_SCORE) break;
+
+            //Move target 
+            target.mPosX = rand() % (SCREEN_WIDTH - target.DOT_WIDTH);
+            target.mPosY = rand() % (SCREEN_HEIGHT - target.DOT_HEIGHT);
+        }
+
+        //Move the dot
+        player.move(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        //Clear screen
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(gRenderer);
+
+        //Render objects
+        player.render(gRenderer);
+        target.render(gRenderer);
+
+        //Update screen
+        SDL_RenderPresent(gRenderer);
     }
 
     //Free resources and close SDL
